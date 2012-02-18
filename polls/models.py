037ef0463bbdbd7f4a1dc40f4d194e0615fc79f0
Gert Jan van Hardeveld <gj@gj-VirtualBox.(none)>
2012-02-17 23:26:45 -0600

    changes to poll models

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,19 @@
+import datetime
+
 from django.db import models
 
 class Poll(models.Model):
     question = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date publish')
+    def __unicode__(self):
+        return self.question
+    def was_published_today(self):
+        return self.pub_date.date() == datetime.date.today()
 
 class Choice(models.Model):
     poll = models.ForeignKey(Poll)
     choice = models.CharField(max_length=200)
     votes = models.IntegerField()
+    def __unicode__(self):
+        return self.question
+
